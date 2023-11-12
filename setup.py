f683d5ecb7b8880142fa3326bab5f9abe56461cf
Nicolás Salcedo <nicolas.salcedo@fau.de>
2023-11-12 14:13:38 +0100

    Deleted "Version" from setup.pu

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages
 
 setup(
     name="math_quiz",
-    version="0.1",
     author="Nicolás Salcedo",
     author_email="nicolas.salcedo@fau.de",
     packages=find_packages(),
